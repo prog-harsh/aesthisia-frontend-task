a55be3a5cedf55d5d96d1319807674eda0b44b73
Harsh Tripathi <ht87374@gmail.com>
2023-06-01 00:09:36 +0530

    Add: form validation

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,14 +1,45 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import hero from "../assets/hero.png";
 import petals from "../assets/petals.png";
 import logo from "../assets/logo.png";
 import Vector from "../assets/Vector.svg";
+import { emailRegex, passwordRegex } from "../utils";
 
 const LoginPage = () => {
   const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [logged, setLogged] = useState(false);
 
   const toggleShowPassword = () => {
     setShow(!show);
+  };
+
+  const validateEmail = () => emailRegex.test(email);
+
+  const validatePassword = () => passwordRegex.test(password);
+  console.log(email);
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    const isValidEmail = validateEmail();
+    const isValidPassword = validatePassword();
+
+    if (!isValidEmail && !isValidPassword) {
+      setError("Invalid Email and Password!");
+      setLogged(false);
+    } else if (!isValidEmail) {
+      setError("Invalid Email!");
+      setLogged(false);
+    } else if (!isValidPassword) {
+      setError("Invalid Password!");
+      setLogged(false);
+    }
+    else{
+      setError("You are logged in successfully!")
+      setLogged(true);
+    }
   };
 
   return (
@@ -51,34 +82,48 @@ const LoginPage = () => {
         </h1>
         <p className="text-lightGrey mb-6">Glad to see you, Again!</p>
 
-        <div className="max-sm:w-4/5 w-[360px]">
+        <form onSubmit={handleLogin} className="max-sm:w-4/5 w-[360px]">
           <div className="border-2 flex mb-5 w-full px-3 py-3 rounded-lg border-lightGrey border-opacity-50  hover:border-opacity-95">
             <input
               type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               className="outline-none bg-transparent h-full flex-grow"
               placeholder="Enter your email"
+              required
             />
           </div>
 
           <div className="border-2 flex w-full px-3 py-3 rounded-lg border-lightGrey border-opacity-50  hover:border-opacity-95">
             <input
               type={show ? "text" : "password"}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               className="outline-none bg-transparent h-full flex-grow"
               placeholder="Enter your password"
+              required
             />
             <img src={Vector} alt="show" onClick={toggleShowPassword} />
           </div>
+          <p className={logged? "text-sm text-green-800" : "text-sm text-red-800"}>{error}</p>
           <p className="text-lightGrey mt-1 text-right text-sm">
             Forgot Password?
           </p>
-          <button className="bg-offblack mt-12 shadow-2xl text-white w-full text-xl font-medium py-2 rounded-lg">
+          <button
+            type="submit"
+            className="bg-offblack mt-12 shadow-2xl text-white w-full text-xl font-medium py-2 rounded-lg"
+          >
             Login
           </button>
           <p className="text-center mt-14 text-sm">
             Don't have an account yet?{" "}
             <span className="text-offcyan cursor-pointer">Sign Up</span>
           </p>
-        </div>
+        </form>
       </div>
     </div>
   );
